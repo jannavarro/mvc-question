245a5c61cf82dcc96b2314aa13e3bdea2278c715
jannavarro <janmnavarro@yahoo.com>
2015-08-28 15:08:19 +0800

    for stackoverflow

---
+++ MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ MvcApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace MvcApplication2.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            string path = HttpContext.Current.Server.MapPath("~/bin");
+
+            foreach (string dll in Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories))
+            {
+                try
+                {
+                    Assembly loadedAssembly = Assembly.LoadFile(dll);
+                    var type = typeof(Interface1);
+                    var types = loadedAssembly.GetTypes().Where(c => type.IsAssignableFrom(c));
+                }
+                catch (Exception e)
+                {
+                }
+
+            }
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
